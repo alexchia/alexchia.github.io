5807e7281f2bbecc59903155c1d4d67afcb0abac
Alexander Chia <ac@alexchia.com>
2015-12-23 02:58:59 +0800

    new content

--- assets/custom/js/script.js
+++ assets/custom/js/script.js
@@ -291,31 +291,5 @@ jQuery(document).ready(function () {
     });
 
 
-
-    /* Flickr feed */
-
-    //jQuery('#basicuse').jflickrfeed({
-    //    limit: 4,
-    //    qstrings: {
-    //        id: '32532032@N06'
-    //    },
-    //    itemTemplate: '<li>' +
-    //    '<a href="{{image_b}}"><img src="{{image_s}}" class="img-rounded" alt="{{title}}" /></a>' +
-    //    '</li>'
-    //});
-
-
-
-    /* Google Analytics */
-
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-40696437-12', 'auto');
-    ga('send', 'pageview');
-
-
 });
 
